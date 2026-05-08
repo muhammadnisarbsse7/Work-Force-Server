856ce07deb985088089d777810ece74a65510302
alwahid786 <wahidforjob@gmail.com>
2026-05-08 15:54:21 +0500

    fix th ecorese error

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5174',
   process.env.CLIENT_URL,
-].filter(Boolean);
+].filter(Boolean).map(url => url.replace(/\/$/, '')); // Remove trailing slashes
 
 app.use(
   cors({
